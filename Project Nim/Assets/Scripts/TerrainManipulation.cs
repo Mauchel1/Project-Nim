2cd5a679ec1aaf2f50b8da4a57d7dc24eaa81d03
Daniel Friedrich <Daniel.Friedrich@hbg.yxlon.com>
2020-11-04 16:47:25 +0100

    first step for terrain manipulation

--- Project Nim/Assets/Scripts/TerrainManipulation.cs
+++ Project Nim/Assets/Scripts/TerrainManipulation.cs
@@ -13,16 +13,43 @@ public class TerrainManipulation : MonoBehaviour
 
     public float radius = 1.5f;
     public float power = 2.0f;
+	
+	
+	RaycastHit hit = new RaycastHit();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+
+
+	    if (Physics.Raycast(ray.origin, ray.direction, out hit))
+	    {
+	    Debug.DrawRay(hit.point, hit.normal, Color.red);
+
+		    if (Input.GetMouseButtonUp(0))
+		    {
+
+			    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			    sphere.transform.position = hit.point;
+			    Mesh mesh = hit.collider.gameObject.GetComponent<Mesh>();
+			    Debug.Log(mesh.vertices.Length);
+				//mesh.transform.position += new Vector3(0, 15f, 0);
+
+		    }
+
+		    if (Input.GetMouseButton(0))
+		    {
+				ModifyMesh();
+		    }
+	    }
     }
+
+	void ModifyMesh()
+	{
+
+	}
+
 }
