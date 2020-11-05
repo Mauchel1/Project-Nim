b308e6aaf5e5aa31af23fa1703f316d48cf0512d
Daniel Friedrich <Daniel.Friedrich@hbg.yxlon.com>
2020-11-05 15:49:35 +0100

    simple mesh manipulation working

--- Project Nim/Assets/Scripts/TerrainManipulation.cs
+++ Project Nim/Assets/Scripts/TerrainManipulation.cs
@@ -11,11 +11,15 @@ public class TerrainManipulation : MonoBehaviour
         MeshNormal
     }
 
-    public float radius = 1.5f;
+
+	public Dictionary<Vector2, Terrainchunk> terrainChunkDict;
+
+    public float radius = 3f;
     public float power = 2.0f;
 	
 	
 	RaycastHit hit = new RaycastHit();
+	private MeshFilter filter;
 
 
     // Update is called once per frame
@@ -28,28 +32,63 @@ public class TerrainManipulation : MonoBehaviour
 	    if (Physics.Raycast(ray.origin, ray.direction, out hit))
 	    {
 	    Debug.DrawRay(hit.point, hit.normal, Color.red);
-
-		    if (Input.GetMouseButtonUp(0))
+		    if (Input.GetMouseButtonDown(0))
 		    {
 
 			    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			    sphere.transform.position = hit.point;
-			    Mesh mesh = hit.collider.gameObject.GetComponent<Mesh>();
-			    Debug.Log(mesh.vertices.Length);
-				//mesh.transform.position += new Vector3(0, 15f, 0);
+			    GameObject chunk = hit.collider.gameObject;
+			    filter = hit.collider.gameObject.GetComponent<MeshFilter>();
+				
+			    //Debug.Log(mesh.vertices.Length);
+			    //mesh.transform.position += new Vector3(0, 15f, 0);
 
 		    }
-
+		    
 		    if (Input.GetMouseButton(0))
 		    {
-				ModifyMesh();
+			    if (filter != null && filter.mesh != null)
+			    {
+					ModifyMesh(filter, hit.point);
+			    }
 		    }
 	    }
     }
 
-	void ModifyMesh()
+	void ModifyMesh(MeshFilter f, Vector3 hitpoint)
 	{
+		Mesh mesh = f.mesh;
+		Vector3[] vertices = mesh.vertices;
+
+		for (int i = 0; i < vertices.Length; ++i)
+		{
+			Vector3 v = f.transform.TransformPoint(vertices[i]);
+			//vertices[i] += hit.normal * Gaussian(v, hit.point, radius);
+			vertices[i] += Vector3.up * Gaussian(v, hit.point, radius);
+
+		}
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds();
+
+		MeshCollider collider = f.GetComponent<MeshCollider>();
+		if (collider != null)
+		{
+			var colliMesh = new Mesh();
+			colliMesh.vertices = mesh.vertices;
+			colliMesh.triangles = mesh.triangles;
+			collider.sharedMesh = colliMesh;
+		}
 
 	}
+
+	static float Gaussian(Vector3 pos, Vector3 mean, float dev)
+	{
+		float x = pos.x - mean.x;
+		float y = pos.y - mean.y;
+		float z = pos.z - mean.z;
+		float n = 1.0f / (2.0f * Mathf.PI * dev * dev);
+		return n * Mathf.Pow(2.718281828f, -(x * x + y * y + z * z) / (2.0f * dev * dev));
+	}
+
 
 }
